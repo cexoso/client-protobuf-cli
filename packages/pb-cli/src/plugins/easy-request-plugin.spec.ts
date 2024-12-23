9b0d5fe9d1aedd5c28f9224e4e378793e00d5da4
xiongjie <jiexiong@futunn.com>
2024-12-23 10:23:58 +0800

    添加简单请求 plugin

--- packages/pb-cli/src/plugins/easy-request-plugin.spec.ts
+++ packages/pb-cli/src/plugins/easy-request-plugin.spec.ts
@@ -1,29 +1,21 @@
 import { describe, it } from 'vitest'
 import { createContainer } from '../container'
-import { Command } from './command'
 import { join } from 'path'
+import { Command } from '../command/command'
 
 const root = join(__dirname, '../../test-protos')
 const dist = join(__dirname, '../../dist')
 
-describe('Command', () => {
-  it('生成所有的文件', async () => {
-    const container = createContainer()
-    const cmd = container.get(Command)
-    await cmd.compileProtos({
-      protoDir: root,
-      outDir: dist,
-      verbose: false,
-      autoClean: true,
-      withPrettier: true,
-      // 把这个改成 false, 会真实的在 dist 目录输出文件
-      dryRun: true,
-    })
-  })
-  it('使用 srpc Header 类编解码', async () => {
+describe('插件', () => {
+  it('插件', async () => {
     // 现在全量生成还有部分问题没有解决
     const container = createContainer()
     const cmd = container.get(Command)
+    cmd.addPlugin({
+      afterGenerate(ctx) {
+        console.log('debugger 🐛 ctx', ctx)
+      },
+    })
     await cmd.compileProtos({
       protoDir: root,
       outDir: dist,
